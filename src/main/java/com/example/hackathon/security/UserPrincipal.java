6b9ec520176a5bb8cd8911cd54489bae40a88eac
Dead-or-Alive0609 <heuiwon716@gmail.com>
2025-08-18 00:55:52 +0900

    feat: 최신 버전 업데이트 (#17)

--- src/main/java/com/example/hackathon/security/UserPrincipal.java
+++ src/main/java/com/example/hackathon/security/UserPrincipal.java
@@ -1,19 +1,19 @@
-// src/main/java/com/example/hackathon/security/UserPrincipal.java
-package com.example.hackathon.security;
+// // src/main/java/com/example/hackathon/security/UserPrincipal.java
+// package com.example.hackathon.security;
 
-import org.springframework.security.core.GrantedAuthority;
+// import org.springframework.security.core.GrantedAuthority;
 
-import java.security.Principal;
-import java.util.Collection;
+// import java.security.Principal;
+// import java.util.Collection;
 
-/**
- * JWT에서 꺼낸 사용자 정보(	id/email/권한	)를 담는 간단한 Principal.
- * 컨트롤러에서 @AuthenticationPrincipal 로 바로 주입받아 사용한다.
- */
-public record UserPrincipal(
-        Integer id,
-        String email,
-        Collection<? extends GrantedAuthority> authorities
-) implements Principal {
-    @Override public String getName() { return email; }
-}
+// /**
+//  * JWT에서 꺼낸 사용자 정보(	id/email/권한	)를 담는 간단한 Principal.
+//  * 컨트롤러에서 @AuthenticationPrincipal 로 바로 주입받아 사용한다.
+//  */
+// public record UserPrincipal(
+//         Integer id,
+//         String email,
+//         Collection<? extends GrantedAuthority> authorities
+// ) implements Principal {
+//     @Override public String getName() { return email; }
+// }
